fb15a58294d107e4ad378328c8e3d721edaf102c
chiku banda <chikubanda@pop-os.localdomain>
2021-12-06 19:57:51 +0100

    created instructor uni

--- src/com/chiku/jdbc/demo/CreateDemo.java
+++ src/com/chiku/jdbc/demo/CreateDemo.java
@@ -1,54 +1,49 @@
 package com.chiku.jdbc.demo;
 
+
 import com.chiku.jdbc.entities.Instructor;
 import com.chiku.jdbc.entities.InstructorDetail;
-import com.chiku.jdbc.entities.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class CreateDemo {
     public static void main(String[] args) {
+
         // create session factory
-        SessionFactory sessionFactory = new Configuration()
-                                            .configure()
-                                            .addAnnotatedClass(Instructor.class)
-                                            .addAnnotatedClass(InstructorDetail.class)
-                                            .buildSessionFactory();
+        SessionFactory sessionFactory =
+                new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Instructor.class)
+                        .addAnnotatedClass(InstructorDetail.class)
+                        .buildSessionFactory();
 
         // get current session
         Session session = sessionFactory.getCurrentSession();
 
         try {
-            /*// create instructor object
-            Instructor instructor = new Instructor("chiku", "banda", "chikubanda.cb@gmail.com");
+            // begin transaction
+            session.beginTransaction();
 
-            // create instructor detail
-            InstructorDetail instructorDetail = new InstructorDetail("youtuve.com", "coding");
+            // create objects
+            Instructor instructor = new Instructor(
+                    "chiku",
+                    "banda",
+                    "chiku@gmail.com"
+            );
+
+            InstructorDetail instructorDetail = new InstructorDetail(
+                    "chiku's channel",
+                    "codding"
+            );
 
             // associate objects
             instructor.setInstructorDetail(instructorDetail);
 
-            // begin transaction
-            session.beginTransaction();
+            // save instructor object
+            session.save(instructor);
 
-            // save the instructor
-            // this will also save the instructor details
-            // because of cascade.ALL
-            System.out.println("Saving instructor" + instructor);
-            session.save(instructor);*/
-
-            /*********************************************/
-
-            // begin transaction
-            session.beginTransaction();
-
-            // retrieve instructor
-            Instructor myInstructor = session.get(Instructor.class, 1);
-
-            System.out.println(myInstructor);
-
-            // commit transaction
+            // persist object
             session.getTransaction().commit();
         }
         finally {
